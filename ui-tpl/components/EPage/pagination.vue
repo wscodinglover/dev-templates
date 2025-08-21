343a75d2d8ca5057e73c32119d8f7e87886c9aff
wscodinglover <xichuang1992@163.com>
2025-08-21 14:35:14 +0800

    feat: add 2 template

---
+++ ui-tpl/components/EPage/pagination.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <e-pagination
+      v-if="pageInfo.show && pageInfo.total > 0"
+      :total="pageInfo.total"
+      :current-page="pageInfo.pageNum"
+      :page-size="pageInfo.pageSize"
+      :page-sizes="pageSizes"
+      @handleSizeChange="handleSizeChange"
+      @handleCurrentChange="handleCurrentChange"
+    ></e-pagination>
+  </div>
+</template>
+<script>
+import EPagination from '../EPagination'
+export default {
+  components: { EPagination },
+  props: {
+    pageInfo: {
+      type: Object,
+      default: () => ({
+        show: true,
+        total: 0,
+        pageNum: 1,
+        pageSize: 20,
+      }),
+    },
+    pageSizes: {
+      type: Array,
+      default: () => [10, 20, 30, 50],
+    },
+  },
+
+  methods: {
+    handleCurrentChange(pageNum) {
+      this.$emit('paginationEvent', {
+        pageNum,
+      })
+    },
+    handleSizeChange(pageSize) {
+      this.$emit('paginationEvent', {
+        pageNum: 1,
+        pageSize,
+      })
+    },
+  },
+
+}
+</script>
---
+++ ui-tpl/src/main.js
@@ -0,0 +1,7 @@
+import EPage from '../components/EPage'
+import ESearch from '../components/ESearch'
+import ETable from '../components/ETable'
+import EPagination from '../components/EPagination'
+import EDateRange from '../components/EDateRange'
+
+export { EPage, ESearch, ETable, EPagination, EDateRange }
